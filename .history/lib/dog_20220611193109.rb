b01c61eeb9df66a49f8d3357acf56513b1a5f7bd
NDICHU <ndichumuriithi@gmail.com>
2022-06-11 19:35:57 +0300

    commit tree b3c43103e43df05f1366f0a669c8e28581152dd9

---
+++ .history/lib/dog_20220611193109.rb
@@ -0,0 +1,20 @@
+class Dog
+    #setter method
+    def name=(dog_name)
+    # assign the @name instance variable a value of 
+    #whatever is passed as an argument
+        @this_dogs_name = dog_name
+    end
+    def name
+        @this_dogs_name
+    end
+end
+lassie = Dog.new
+lassie.name = "Lassie"
+puts lassie.name
+
+odie = Dog.new
+# call the Dog#name= method with a value of "Odie"
+odie.name = "Odie"
+# this is equivalent to calling odie.name=("Odie")
+puts odie.name
